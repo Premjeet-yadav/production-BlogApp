1db3c1af73f147ddf5f42903b3cdf6f497b37bc7
Premjeet-yadav <ypremjeet360@gmail.com>
2023-09-19 04:08:41 +0530

    more updates

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const morgan = require("morgan");
-const colors = require("colors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const path = require("path");
@@ -22,7 +20,7 @@ const app = express();
 //middelwares
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 
 //routes
 app.use("/api/v1/user", userRoutes);
@@ -40,8 +38,5 @@ app.get('*',(req,res)=>{
 const PORT = process.env.PORT || 8080;
 //listen
 app.listen(PORT, () => {
-  console.log(
-    `Server Running on ${process.env.DEV_MODE} mode port no ${PORT}`.bgCyan
-      .white
-  );
+  console.log(`Server Running on mode port no ${PORT}`);
 });
